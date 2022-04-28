2669cc0b08f7ee0f32fc44d807060f5df68b961c
Benoit Lamonica <ben33127@gmail.com>
2022-04-28 17:24:57 +0200

    add: first layout

---
+++ src/components/global/Grid.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ src/components/global/Parallax.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :style="{top}">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+	setup() {
+		const top = ref(0)
+		const speed = 3
+		const scrollListener = () => {
+			top.value = -window.scrollY / speed + 'px'
+		}
+
+		return {
+			scrollListener,
+			top
+		}
+	},
+
+	mounted() {
+		window.addEventListener('scroll', this.scrollListener)
+	},
+
+	unmounted() {
+		window.removeEventListener('scroll', this.scrollListener)
+	}
+}
+</script>
