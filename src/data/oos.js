e6aab0dd411219b7779c003af221e169c68ed1d3
Benoit Lamonica <ben33127@gmail.com>
2022-05-24 23:25:09 +0200

    add: content to oos

--- src/data/oos.js
+++ src/data/oos.js
@@ -4,7 +4,7 @@ export default [
 		color: 'main',
 		content: [
 			{
-				video: '9bYpk75JnZU'
+				video: 'OrxmtDw4pVI'
 			},
 			{
 				type: 'h3',
@@ -58,11 +58,11 @@ export default [
 		],
 	},
 	{
-		name: 'The Vue Documentary',
+		name: 'Flutter',
 		color: 'main',
 		content: [
 			{
-				video: '9bYpk75JnZU'
+				video: '80pRyn7fZRk'
 			},
 			{
 				type: 'h3',
@@ -70,39 +70,31 @@ export default [
 			},
 			{
 				type: 'p',
-				text: 'Dans ce reportage on peut suivre le parcours de Evan You, créateur de Vue et de ses librairies externe. Un parcours atypique pour un developpeur chinois. Un héros dans son pays',
+				text: 'Ce talk nous parle de Flutter, une technologie en plein essor qui monte au fur et à mesure des années.',
 			},
 			{
 				type: 'h3',
-				text: 'Web Dévellopeur'
+				text: 'Une Technologie pas si récente ...'
 			},
 			{
 				type: 'p',
-				text: 'Evan You a commencé en tant que Web développeur junior autodidacte, il aimait faire des applications Web inspiré des vraies applications mobiles, créé à l\'époque avec des langages très difficile à appréhender pour des débutants. En s\'inspirant des animations mobiles, il est tombé sur une animation qu\'il a trouvé très inspirante. Cette animation c\'était celle du slide sur le premier iPhone, lorsqu\'on passe son pouce sur un objet l\'objet glisse en même tant que le pouce. Il s\'est alors lancé un défi : reproduire l\'animation avec la technologie Web en utilisant le JavaScript.',
+				text: 'Flutter est né en 2013 de l\'initiative de Google, l\'idée étant de réunir sur une seule plate-forme la création d\'application Multi device. Il utilise un nouveau langage, nommé DART. C\'est un langage crée aussi par google dans l\'optique d\'être compilé dans de multiples langage mobile. Cependant Flutter ne fut pas l\'unanimité directement mais bien après son lancement. Ce n\'est qu\'à partir de Flutter 2.0 que la hype autour de ce framework commença à emmerger',
 			},
 			{
 				type: 'h3',
-				text: 'Google'
+				text: 'Flutter c\'est quoi ?'
 			},
 			{
 				type: 'p',
-				text: 'Finalement après quelques essais non fructueux, il réussi à mettre en place (quasiment à l\'identique) celle présente sur les mobiles iPhone. Grâce à ça Google le recruta dans sa team des cinq, cette équipe permettait à Google d\'imaginer des concepts pouvant être utilisé dans les années qui suivent c\'est en quelque sorte les \'\'concepts cars\'\' des futurs sites qui serait présent sur le Net. En travaillant pour cette équipe, Evan s\'est rendu compte des limitations que présentaient les Framework actuel (nous sommes en 2013), ce faisant il songeat à créer son propre framework pour acceder à des fonctionnalités qui ne pouvait actuellement pas utiliser due aux limitations des technologies de l\'époque',
+				text: 'C\'est un Framework basé sur le langage DART comme dit précédemment, Il réunis en un seul bloc tous les outils nécessaires pour créer des applications à la fois sur iOS mais aussi sur Android. Mais pas seulement, en effet Flutter peut aussi créer des applications directement sur le Web. Ces applications sont appeler Web–app, elle deviennent de plus en plus une norme dans les entreprises car elles présentent tous les avantages d\'une applicaiton native tout en étant hébergé par un serveur web. De plus la notion de connexion internet obligatoire est de moins en moins d\'actualité étant donnée l\'emmergence des PWA (Prgressives Web App).',
 			},
 			{
 				type: 'h3',
-				text: 'Le début du commencement'
+				text: 'Avantages ?'
 			},
 			{
 				type: 'p',
-				text: 'À partir de là, Evan partie de Google pour aller dans une entreprise qui fabriquait des Framework, Parallèlement il continue toujours à travailler sur Vue.js. D\'ailleurs c\'est à ce moment-là du documentaire que l\'on apprend d\'où vient le nom Vue.js : il vient tout simplement du fait que, pour afficher une page on utilise le terme View en anglais. Mais comme ce terme là était déjà pris par d\'autres librairies JavaScript il se mit alors à essayer de trouver un nouveau nom dans d\'autres langues. C\'est alors tout simplement qu\'il est tombé sur la traduction française de ce même mot : Vue. À ce moment-là de sa vie il était à mi-temps sur son Framework est à moitié dans son boulot et il prit la décision d\'arrêter son travail pour pouvoir se consacrer pleinement à sa passion : Vue',
-			},
-			{
-				type: 'h3',
-				text: 'Laravel, un coup de pouce'
-			},
-			{
-				type: 'p',
-				text: 'Évidemment il y a eu un élément déclencheur, ce fut lorsque le patron de Laravel (un grand Framework PHP) fit un tweet à propos de Vue. C\'est alors que toute la communauté de Laravel commeça à utiliser Vue, qui selon l\'opinion générale était un framework très facile d\'accès et simple d\'utilisation. À partir de ce moment-là Evan You consacra cent pour cent de son temps au developpement de son Framework',
+				text: 'Contrairement au langage natif de programmation pour les mobiles, Flutter possède une feature très intéressante. Cette feature est tout simplement ce que l\'on appelle le Hot Reload. Quand on Craft une application depuis un langage natif, on ne peut pas prévisualiser notre vue directement sans avoir à recompiler toute l\'application. Grâce à Flutter nous pouvons visualiser les changements effectués au cours de nos divers changements de code en direct. C\'est une très grande avancée dans la création d\'applications et surtout un énorme gain de temps. L\'avantage que présente Flutter c\'est qu\'il est quasiment aussi performant que le langage natif, en effet celui-ci est compilé grâce a un moteur made in Google dans les langages natif des devices respectifs. Si on devait résumer Flutter en une seule phrase c\'est la combinaison de toutes les meilleurs technologies possibles sur mobile en un seul outil.',
 			},
 			{
 				type: 'h3',
@@ -110,13 +102,12 @@ export default [
 			},
 			{
 				type: 'p',
-				text: 'Pour conclure, je dirais que ce reportage m\'a tout particulièrement touché, car c\'est ce genre d\'histoire qui me donne envie et qui me passionne pour ce métier. En effet quand on voit ce genre de parcours on a qu\'une seule envie c\'est continuer à progresser et surtout ne jamais arrêter de se renseigner et d\'étudier les technologies nouvelles.',
+				text: 'De mon point de vue, je pense réellement que Flutter a un très grand avenir. J\'aime beaucoup la façon dont il fait la liaison entre les développeurs Web et les développeur mobile, car pour beaucoup de ses fonctionnalités il empreinte le meilleur des deux mondes. C\'est d\'ailleurs cette vidéo qui m\'as donné envie de me renseigner et d\'utiliser Flutter pour mes projets futurs',
 			},
-      
 		],
 	},
 	{
-		name: 'The Vue Documentary',
+		name: 'Start-ups en E-stonia',
 		color: 'main',
 		content: [
 			{
@@ -128,39 +119,31 @@ export default [
 			},
 			{
 				type: 'p',
-				text: 'Dans ce reportage on peut suivre le parcours de Evan You, créateur de Vue et de ses librairies externe. Un parcours atypique pour un developpeur chinois. Un héros dans son pays',
+				text: 'Ce documentaire nous montre / présente le système mis en place par l\'Estonie pour digitaliser les institutions et faciliter l\'etablissement de nouvelles sociétées dans celui-ci',
 			},
 			{
 				type: 'h3',
-				text: 'Web Dévellopeur'
+				text: 'L\'essor de l\'e-stonie'
 			},
 			{
 				type: 'p',
-				text: 'Evan You a commencé en tant que Web développeur junior autodidacte, il aimait faire des applications Web inspiré des vraies applications mobiles, créé à l\'époque avec des langages très difficile à appréhender pour des débutants. En s\'inspirant des animations mobiles, il est tombé sur une animation qu\'il a trouvé très inspirante. Cette animation c\'était celle du slide sur le premier iPhone, lorsqu\'on passe son pouce sur un objet l\'objet glisse en même tant que le pouce. Il s\'est alors lancé un défi : reproduire l\'animation avec la technologie Web en utilisant le JavaScript.',
+				text: 'Comme vu dans le documentaire l\'Estonie est un pays en plein essor numérique. Ils ont eu l\'immense ingéniosité de penser leur lieu de vie comme une immense start up digitale. En effet dans ce pays on peut facilement acquérir la nationalité avec une carte de séjour \'digitale\'. Le but selon eux était de créer la silicon valley de l\'europe. C\'est peu dire car pour concurencer les américain sur ce terrain il faut en vouloir ...',
 			},
 			{
 				type: 'h3',
-				text: 'Google'
+				text: 'Un pays en avance ?'
 			},
 			{
 				type: 'p',
-				text: 'Finalement après quelques essais non fructueux, il réussi à mettre en place (quasiment à l\'identique) celle présente sur les mobiles iPhone. Grâce à ça Google le recruta dans sa team des cinq, cette équipe permettait à Google d\'imaginer des concepts pouvant être utilisé dans les années qui suivent c\'est en quelque sorte les \'\'concepts cars\'\' des futurs sites qui serait présent sur le Net. En travaillant pour cette équipe, Evan s\'est rendu compte des limitations que présentaient les Framework actuel (nous sommes en 2013), ce faisant il songeat à créer son propre framework pour acceder à des fonctionnalités qui ne pouvait actuellement pas utiliser due aux limitations des technologies de l\'époque',
+				text: 'Mais alors peut on dire que l\'estonie est en avance sur son temps ? Tout depend des points de vue je dirais. Partons du principe que pour un pays de cette taille c\'est quand même très osé. Cependant si on regarde les autres pays par rapport à celui-ci, on peut voir qu\'il est sacrément en avance. Dans ce documentaire on peut aussi voir que beaucoup d\'entreprises de l\'étranger s\'intérresent aux possibilitées qu\'offrent un systeme comme celui-ci. Les méthodes employées sont très américaines et on sent un arrière goût de start up géante (à l\'echelle d\'un pays).',
 			},
 			{
 				type: 'h3',
-				text: 'Le début du commencement'
+				text: 'Les avantages ?'
 			},
 			{
 				type: 'p',
-				text: 'À partir de là, Evan partie de Google pour aller dans une entreprise qui fabriquait des Framework, Parallèlement il continue toujours à travailler sur Vue.js. D\'ailleurs c\'est à ce moment-là du documentaire que l\'on apprend d\'où vient le nom Vue.js : il vient tout simplement du fait que, pour afficher une page on utilise le terme View en anglais. Mais comme ce terme là était déjà pris par d\'autres librairies JavaScript il se mit alors à essayer de trouver un nouveau nom dans d\'autres langues. C\'est alors tout simplement qu\'il est tombé sur la traduction française de ce même mot : Vue. À ce moment-là de sa vie il était à mi-temps sur son Framework est à moitié dans son boulot et il prit la décision d\'arrêter son travail pour pouvoir se consacrer pleinement à sa passion : Vue',
-			},
-			{
-				type: 'h3',
-				text: 'Laravel, un coup de pouce'
-			},
-			{
-				type: 'p',
-				text: 'Évidemment il y a eu un élément déclencheur, ce fut lorsque le patron de Laravel (un grand Framework PHP) fit un tweet à propos de Vue. C\'est alors que toute la communauté de Laravel commeça à utiliser Vue, qui selon l\'opinion générale était un framework très facile d\'accès et simple d\'utilisation. À partir de ce moment-là Evan You consacra cent pour cent de son temps au developpement de son Framework',
+				text: 'Après avoir vu le documentaire, je me suis demandé quelles seraient les avantages à avoir un système comme celui-ci. Il m\'a paru évident que tout centraliser sur Internet ne portait pas que des avantages. En effet centraliser toutes les données comme dit dans le documentaire possède de grands risques car les celles-ci étant stocké sur des serveurs elles sont très sensible aux hacks. Il faut donc vivre et s\'adapter avec ce nouveau mode de vie. C\'est-à-dire investir dans la protection des données au maximum, ce sujet étant d\'ailleurs très sensible aujourd\'hui',
 			},
 			{
 				type: 'h3',
@@ -168,7 +151,7 @@ export default [
 			},
 			{
 				type: 'p',
-				text: 'Pour conclure, je dirais que ce reportage m\'a tout particulièrement touché, car c\'est ce genre d\'histoire qui me donne envie et qui me passionne pour ce métier. En effet quand on voit ce genre de parcours on a qu\'une seule envie c\'est continuer à progresser et surtout ne jamais arrêter de se renseigner et d\'étudier les technologies nouvelles.',
+				text: 'J\'avoue avoir été impresionné par le contenu de ce documentaire, avant ça je n\'avais pas du tout conscience de l\'existence de ce genre de pratique en Europe. Je suis assez neutre sur la question, je demande a voir dans le futur les avancées et les potentielle faille de ce système',
 			},
       
 		],
