79c4891e512cc51583eda370b3966f6de1a6b981
Benoit Lamonica <ben33127@gmail.com>
2022-05-24 23:46:59 +0200

    add: oos

--- src/pages/OosPage.vue
+++ src/pages/OosPage.vue
@@ -1,19 +1,50 @@
 <template>
   <div class="py-4 px-4">
     <h2 class="text-8xl uppercase font-extrabold bg-gradient-to-t to-indigo-500 from-indigo-400 text-transparent bg-clip-text text-center">
-      Out of School
+      Out of School {{ currentActiveIndex + 1 }} / 3
     </h2>
     <div class="text-center">
       Benoit Lamonica - 2022
     </div>
-    <div class=" bg-indigo-50 rounded-lg shadow-md shadow-indigo-400 max-w-screen-xl mx-auto mt-20 p-8">
-      <OOS
-        v-for="{name, color, content}, i in oos"
-        :key="i"
-        :content="content"
-        :color="color"
-        :title="name"
-      />
+
+    <div class="flex justify-between items-center m-auto fixed inset-8">
+      <button
+        v-if="currentActiveIndex > 0"
+        class="mr-auto p-4 bg-indigo-600 text-white font-extrabold rounded shadow-sm shadow-indigo-900"
+        @click="direction = 'previous'; currentActiveIndex--"
+      >
+        Previous
+      </button>
+      <button
+        v-if="currentActiveIndex < 2"
+        class="ml-auto p-4 bg-indigo-600 text-white font-extrabold rounded shadow-sm shadow-indigo-900"
+        @click="direction = 'next'; currentActiveIndex++"
+      >
+        Next
+      </button>
+    </div>
+    <div class="relative overflow-hidden max-w-screen-xl mx-auto">
+      <transition-group
+        enter-active-class="transition duration-1000"
+        :enter-from-class="`transform ${direction === 'next' && 'translate-x-full'} ${direction === 'previous' && '-translate-x-full'}`"
+        enter-to-class="transform translate-x-0"
+        leave-active-class="transition duration-1000 absolute top-0 left-0 bottom-0 right-0 w-full"
+        leave-from-class="transform translate-x-0"
+        :leave-to-class="`transform ${direction === 'next' && '-translate-x-full'} ${direction === 'previous' && 'translate-x-full'}`"
+      >
+        <div
+          v-for="{name, color, content}, i in oos"
+          v-show="currentActiveIndex === i"
+          :key="i"
+          class="bg-indigo-50 rounded-lg shadow-md shadow-indigo-400 max-w-screen-xl mx-auto mt-20 p-8"
+        >
+          <OOS
+            :content="content"
+            :color="color"
+            :title="name"
+          />
+        </div>
+      </transition-group>
     </div>
   </div>
 </template>
@@ -21,10 +52,18 @@
 <script>
 import oos from '@/data/oos'
 import OOS from '@/components/OOS.vue'
+import { ref } from '@vue/reactivity'
+
 export default {
 	components: { OOS },
+
 	setup() {
+		const currentActiveIndex = ref(0)
+		const direction = ref('')
+
 		return {
+			direction,
+			currentActiveIndex,
 			oos
 		}
 	},
